b0282463b6b6f5614b1f7d1776ddceae9432e9eb
Bai Sun <Cooper@xebni.com>
2021-07-30 16:27:13 +1200

    用户可以删除自己发的话题

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -43,15 +43,23 @@
             {!! $topic->body !!}
           </div>
 
-          <div class="operate">
-            <hr>
-            <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
-              <i class="far fa-edit"></i> Edit
-            </a>
-            <a href="#" class="btn btn-outline-secondary btn-sm" role="button">
-              <i class="far fa-trash-alt"></i> Delete
-            </a>
-          </div>
+          @can('update', $topic)
+            <div class="operate">
+              <hr>
+              <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
+                <i class="far fa-edit"></i> Edit
+              </a>
+              <form action="{{ route('topics.destroy', $topic->id) }}" method="post"
+                    style="display: inline-block;"
+                    onsubmit="return confirm('Are you sure you want to delete this post?');">
+                {{ csrf_field() }}
+                {{ method_field('DELETE') }}
+                <button type="submit" class="btn btn-outline-secondary btn-sm">
+                  <i class="far fa-trash-alt"></i> Delete
+                </button>
+              </form>
+            </div>
+          @endcan
 
         </div>
       </div>
