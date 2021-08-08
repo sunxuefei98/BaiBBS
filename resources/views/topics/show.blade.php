c458f138c6abf94fedbe898fbd51e76e4cd57a18
Bai Sun <Cooper@xebni.com>
2021-08-09 11:36:21 +1200

    注册验证码

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -39,7 +39,7 @@
             {{ $topic->reply_count }}
           </div>
 
-          <div class="topic-body mt-4 mb-4">
+          <div class="topic-body">
             {!! $topic->body !!}
           </div>
 
