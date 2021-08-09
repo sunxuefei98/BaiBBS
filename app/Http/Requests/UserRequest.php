50c34a744dd1e9c397f8ebd52c8ebeef06b10e2b
Bai Sun <Cooper@xebni.com>
2021-08-09 14:00:49 +1200

    限制头像分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -18,12 +18,15 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
+            'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=208,min_height=208',
         ];
     }
 
     public function messages()
     {
         return [
+            'avatar.mimes' =>'The avatar must be a picture in jpeg, bmp, png, gif format',
+            'avatar.dimensions' => 'The sharpness of the picture is not enough, both width and height need to be 208px or more',
             'name.unique' => 'This username is already taken. Please choose another name.',
             'name.regex' => 'Usernames can only comprise letters (a-z), numbers (0-9), dash and underscore.',
             'name.between' => 'Must be between 3-25 characters long.',
