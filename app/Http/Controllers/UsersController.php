ea8dae7768fc08889c4bf3ec462312affd7946e2
Bai Sun <Cooper@xebni.com>
2021-08-09 14:23:18 +1200

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,13 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    //使用 Laravel 提供身份验证（Auth）中间件来过滤未登录用户的 edit, update 动作
+    //except意为：除了此处指定的动作以外，所有其他动作都必须登录用户才能访问，类似于黑名单的过滤机制
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     //    增加 show 方法来处理个人页面的展示 (Your Profile)
     public function show(User $user)
     {
@@ -18,11 +25,13 @@ class UsersController extends Controller
     //    增加 edit 方法来处理个人页面的展示 (Setting)
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('user.edit', compact('user'));
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
